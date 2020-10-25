16a6f17c2381dc75b79e512fa1b9391f77e155e6
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2020-10-25 09:52:35 +0600

    link :
    https://codeforces.com/contest/50/problem/A

---
+++ Dominopiling.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+//Solver : Nahin the coder
+//Date   :31-01-2020
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    int result;
+    cin>>n>>m;
+    result = (n*m)/2;
+    cout<<result<<endl;
+
+
+    return 0;
+}
