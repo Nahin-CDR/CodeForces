fa7e6e8888f0e1d8f7608dfe0f436103e5ae632b
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-02-01 16:53:24 +0600

    link :
    https://codeforces.com/contest/199/problem/A

---
+++ A. Hexadecimal's theorem.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Solver : Nahin the Coder
+// Date   : 01- 02 - 2021
+ 
+ 
+int main()
+{
+    int n;
+    cin>>n;
+   
+    cout<<0<<" "<<0<<" "<<n<<endl;
+	
+ 
+    
+    return 0;
+}
