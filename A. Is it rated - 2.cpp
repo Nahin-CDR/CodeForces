40c5f705fecf892713519ea9a7a5263e51aed96f
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-04-02 09:50:25 +0600

    link :
    https://codeforces.com/contest/1505/problem/A

---
+++ A. Is it rated - 2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+//Solver : Nahin the Coder
+//Date   : 01 - 04 - 2021
+ 
+ 
+int main()
+{
+	
+	
+	#ifdef _DEBUG
+	freopen("input.txt","r",stdin);
+	#endif
+	string s;
+	while(getline(cin,s))
+	{
+		
+	
+		cout<<"NO\n";
+	}
+	
+	
+	return 0;
+}
+ 
