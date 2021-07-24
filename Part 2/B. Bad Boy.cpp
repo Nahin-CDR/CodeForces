c3af413bbb70b0e33be6701df77766abbdf35512
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-07-24 12:29:30 +0600

    link :
    https://codeforces.com/contest/1537/problem/B

---
+++ Part 2/B. Bad Boy.cpp
@@ -0,0 +1,21 @@
+//Nahin CDR
+//Date : 24 - 07 - 2021
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+
+int main()
+{
+	ll test;
+	cin>>test;
+	
+	while(test--)
+	{
+	    ll row,coloumn,x,y;
+		cin>>row>>coloumn>>x>>y;
+		cout<<1<<" "<<1<<" "<<row<<" "<<coloumn<<"\n";	
+	}
+	return 0;
+}
