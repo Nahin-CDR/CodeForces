7babaf3f7f6c73040943f88d009773586632efdd
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-08-02 23:17:15 +0600

    link :
    https://codeforces.com/contest/1549/problem/A

---
+++ Solved by Python/A. Gregor and Cryptography.py
@@ -0,0 +1,13 @@
+# Nahin the Coder
+# Date : 02 - 08 - 2021
+
+
+testcase = int(input())
+while testcase > 0:
+    n = int(input())
+    if n == 5:
+        print(2, " ", 4)
+    else:
+        p = int(n-1)
+        print(p//2, ' ',p)
+    testcase = testcase - 1
