39cb2860ee79424401ab5af6a436aab53d6aca50
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-08-03 16:16:37 +0600

    link

---
+++ Part 2/A. Strange Partition.cpp
@@ -0,0 +1,29 @@
+// Solver : Nahin the Coder
+// Date : 03 - 07 - 2021
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ll test;
+    cin>>test;
+    while(test--)
+    {
+        ll n,x;
+        ll sum1 = 0;
+        ll sum2 = 0;
+        cin>>n>>x;
+        for(ll i = 1; i <= n; i++)
+        {
+            ll k;
+            cin>>k;
+            sum1 = sum1 + (k+x-1)/x;
+            sum2 = sum2 + k;
+        }
+        cout<<(sum2+x-1)/x<<" "<<sum1<<"\n";
+
+    }
+    return 0;
+}
